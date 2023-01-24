00e0c65b1541e272d57209327bbea444615062c5
Dakota Kim <codingvampire@gmail.com>
2023-01-24 17:40:08 -0500

    Initial Commit

---
+++ watchOSAppwriteTest Watch App/watchOSAppwriteTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchOSAppwriteTestApp.swift
+//  watchOSAppwriteTest Watch App
+//
+//  Created by Dakota Kim on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct watchOSAppwriteTest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
